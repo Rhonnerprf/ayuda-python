634512a647b93a7631a72e6154ef64ba767a8b66
Rhonner Ramírez <61910683+rhonnerprf@users.noreply.github.com>
2022-11-18 23:15:25 -0500

    Add files via upload

---
+++ matriz-3-3.py
@@ -0,0 +1,22 @@
+'''
+Realiza un programa que capture los valores de una matriz de 3 x 3 y te imprima en la pantalla la suma de la diagonal.
+'''
+
+fila = []
+matriz = []
+dimension = 3
+
+for i in range(dimension):
+    for j in range(dimension):
+        fila.append(input("Elemento " + str(i+1) + str(j+1) + " :"))
+    matriz.append(fila)
+    fila = []
+
+sum = 0
+for i in range(dimension):
+    for j in range(dimension):
+        if i == j:
+            sum = sum + float(matriz[i][j])
+
+print("Matriz =", matriz)
+print("La suma de los elementos es", sum)
